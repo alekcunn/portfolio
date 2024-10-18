ec8897e00dcaa8f4b718ef5e8cf42d6c8e4642f0
Alex Cunningham <alexcunn@k9alk.love>
2024-10-18 07:40:47 +0000

    needed absolute path

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile('main.html')
+  res.sendFile(path.join(__dirname,  
+    '../public/main.html')
+  )
 });
 
 module.exports = router;
